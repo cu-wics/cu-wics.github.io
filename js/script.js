714d3d763b6eb12adbc7658d8ec67ecc60b4ee04
Jason Le <le.kent.jason@gmail.com>
2019-02-04 21:14:24 -0500

    fixed navbar button on mobile and desktop

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ function typing(id, text) {
 }
 
 function myFunction() {
-    var x = document.getElementsByClassName("topnav");
+    var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
       x.className += " responsive";
     } else {
